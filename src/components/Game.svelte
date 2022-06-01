1c0bdc407be6b967a6150a726a6d0c069d3682b6
Orsailius <sumemail8472@gmail.com>
2022-06-01 13:23:47 -0400

    stuuf

---
+++ src/components/Game.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let game;
+</script>
+
+<div>
+    <h2 class="text-4xl font-bold pt-5">
+        {game.name}
+    </h2>
+    <p class="text-lg pt-4 pb-5">
+        {game.body}
+    </p>
+    <div>
+        <!--<video width="1000" controls>
+            <track kind="captions">
+			<source src={game.video} type="video/ogg">
+			Your browser does not support the video tag.
+		</video>-->
+        <iframe title="video" width="1000" height="500" src={game.video}>
+		</iframe>
+    </div>
+</div>
---
+++ src/components/GameShowcase.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let games;
+
+    import Game from "./Game.svelte";
+
+    var currentGame = games[0].name;
+
+    function handleClick(name)
+    {
+		currentGame = name;
+	}
+</script>
+
+<div class="flex flex-row hero">
+    <div class="w-1/5 pl-15">
+       <div class="grid grid-cols-1">
+           <ul class="menu p-4 overflow-y-auto bg-base-100 text-base-content">
+                {#each games as {name}, i}
+                    <li><button class="btn glass" on:click={() => handleClick(name)} >{name}</button></li>
+                {/each}
+           </ul>  
+       </div>
+   </div>
+   <div class="divider divider-horizontal"></div>
+   <div class="w-4/5 pr-4 pb-10">		
+        {#each games as game}
+            {#if game.name == currentGame}
+                <Game game={game}/>
+            {/if}
+        {/each}
+    </div>
+   </div>
